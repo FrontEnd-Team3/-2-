c80e4d3ee5a8a96ac0b878a4d9236648da2ae48c
yunseungbin <08jmda@gmail.com>
2023-04-08 01:31:02 +0900

    베스트 풀이

---
+++ q1.js
@@ -0,0 +1,30 @@
+/*
+변수 위치 찾기
+변수 a는 main 함수 안에서 사용된다
+변수 b는 dom api selector로 다른 이벤트에서 사용된다
+변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다
+변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.
+변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.
+*/
+
+//정우님의 풀이
+
+function solution(c) {
+  let d = c + 100;
+  return d;
+}
+
+function parse() {
+  let b = document.querySelector("something");
+  b.addEventListener("click", function () {
+    b.style.display = "none";
+  });
+}
+
+function main(action) {
+  let a = 10;
+  let e = action + a;
+  return e;
+}
+
+console.log(main(solution(100)));
